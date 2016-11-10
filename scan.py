5e3ed9aa30a50858fe7ad91fe3a93f4e1ea91068
UgoOV <ugo.piqueras@openvalue.fr>
2016-11-10 14:19:55 +0100

    Ajout Contour pour avoir tout le ticket
    Rajout d'un contour:
    python scan2.py --image images/20161109-0959-9752-402881196-1_prerotated.jpg --contour True

--- scan.py
+++ scan.py
@@ -1,3 +1,6 @@
+
+# coding: utf-8
+
 # USAGE
 # python scan.py --image images/page.jpg 
 
@@ -10,48 +13,59 @@ import argparse
 import cv2
 from PIL import Image
 
+
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required = True,
-	help = "Path to the image to be scanned")
+ap.add_argument("-i", "--image", required = True, help = "Path to the image to be scanned")
+ap.add_argument("-cont", "--contour", required = True, help = "True if you want to add contour")
 args = vars(ap.parse_args())
+
 
 # load the image and compute the ratio of the old height
 # to the new height, clone it, and resize it
 image = cv2.imread(args["image"])
+print args["contour"]
+if args["contour"]:
+    image=cv2.copyMakeBorder(image,10,10,10,10,cv2.BORDER_CONSTANT,value=[0,0,0]) 
 ratio = image.shape[0] / 500.0
+#ratio=1.
 orig = image.copy()
 image = imutils.resize(image, height = 500)
 
 # convert the image to grayscale, blur it, and find edges
 # in the image
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-gray = cv2.GaussianBlur(gray, (5, 5), 0)
+#gray = cv2.GaussianBlur(gray, (5, 5), 0)
 edged = cv2.Canny(gray, 75, 200)
 
 # show the original image and the edge detected image
 print "STEP 1: Edge Detection"
 cv2.imshow("Image", image)
+cv2.imshow("Gray", gray)
 cv2.imshow("Edged", edged)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
+
 # find the contours in the edged image, keeping only the
 # largest ones, and initialize the screen contour
-(cnts, _) = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:5]
+(cnts, _) = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cnts = sorted(cnts, key = cv2.contourArea, reverse = True)
+
+print "cnts :", len(cnts)
 
 # loop over the contours
 for c in cnts:
-	# approximate the contour
-	peri = cv2.arcLength(c, True)
-	approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+    # approximate the contour
+    peri = cv2.arcLength(c, True)
+    approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+    print "Approx :", len(approx)
 
-	# if our approximated contour has four points, then we
-	# can assume that we have found our screen
-	if len(approx) == 4:
-		screenCnt = approx
-		break
+    # if our approximated contour has four points, then we
+    # can assume that we have found our screen
+    if len(approx) == 4:
+        screenCnt = approx
+        break
 
 # show the contour (outline) of the piece of paper
 print "STEP 2: Find contours of paper"
@@ -60,9 +74,11 @@ cv2.imshow("Outline", image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
+
 # apply the four point transform to obtain a top-down
 # view of the original image
 warped = four_point_transform(orig, screenCnt.reshape(4, 2) * ratio)
+
 
 # convert the warped image to grayscale, then threshold it
 # to give it that 'black and white' paper effect
@@ -78,3 +94,8 @@ cv2.waitKey(0)
 
 # Save the grayscale image to jpg
 cv2.imwrite('myimage.jpg', warped)
+
+
+
+
+
